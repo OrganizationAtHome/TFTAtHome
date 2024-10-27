9f5605b8f2dd30edbf7732e593dd3108ab974b37
GZimmer <kristianz1000@gmail.com>
2024-10-27 12:28:53 +0100

    Increased slide speed

--- tftathome/character_body_2d_2.gd
+++ tftathome/character_body_2d_2.gd
@@ -3,5 +3,5 @@ extends CharacterBody2D
  
 func _physics_process(_delta):
 	if is_multiplayer_authority():
-		velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * 10
+		velocity = Input.get_vector("ui_left","ui_right","ui_up","ui_down") * 400
 	move_and_slide()
