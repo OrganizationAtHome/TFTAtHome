354e5ade0fc76f0213f260f2ae0f4ef42f154cf5
GZimmer <kristianz1000@gmail.com>
2024-10-27 22:36:37 +0100

    Update HomeScreen.cs

--- tftathome/scenes/phases/HomeScreen.cs
+++ tftathome/scenes/phases/HomeScreen.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
 using TFTAtHome.models;
+using TFTAtHome.storage;
+using TFTAtHome.util;
 
 public partial class HomeScreen : Node2D
 {
@@ -22,10 +24,17 @@ public partial class HomeScreen : Node2D
     public void _on_join_pressed()
     {
         GameManager.JoinServer();
+        SwitchScene();
     }
 
     public void _on_start_server_pressed()
     {
         GameManager.HostServer();
+        SwitchScene();
+    }
+
+    public void SwitchScene()
+    {
+        SceneUtil.SwitchScene("PreGameScene");
     }
 }
