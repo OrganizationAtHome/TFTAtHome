3662b2c365514ff7140c381bce6f11c965b59eda
GZimmer <kristianz1000@gmail.com>
2024-11-10 19:04:36 +0100

    Fixed cursed code

--- tftathome/scenes/models/CardArea2D.cs
+++ tftathome/scenes/models/CardArea2D.cs
@@ -6,8 +6,7 @@ public partial class CardArea2D : Area2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		InputPickable = true;
-		// this.InputEvent += (viewport, @event, shapeIdx) => CardClicked(viewport, @event, shapeIdx);
+
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,20 +14,15 @@ public partial class CardArea2D : Area2D
 	{
 	}
 
-	private void CardClicked(Node viewport, InputEvent @event, long shapeIdx)
-	{
-        GD.Print("You clicked me retard");
-        if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left)
-		{
-			GD.Print("You clicked me correctly");
-		}
-	}
-
     public override void _InputEvent(Viewport viewport, InputEvent @event, int shapeIdx)
     {
         if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed && mouseEvent.ButtonIndex == MouseButton.Left)
         {
-            GD.Print("You clicked me correctly");
+            GD.Print("You clicked me correctly" + @event.GetInstanceId);
+        }
+        if (@event is InputEventMouseButton mouseEvent2 && mouseEvent2.Pressed && mouseEvent2.ButtonIndex == MouseButton.Right)
+        {
+            GD.Print("YEEET");
         }
     }
 }
